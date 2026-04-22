53627d846e355043db6fd97ab20833c48746f41c
chrisb20100 <chriswes053@gmail.com>
2026-04-22 09:37:38 -0400

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+
+    float xRotation = 0f;
+
+    public Transform cameraHolder;
+
+    void Update()
+    {
+        Move();
+        Look();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
