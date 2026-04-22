dd582884f4de444c7fdc4aad6adc50991b0695b8
chrisb20100 <chriswes053@gmail.com>
2026-04-22 09:38:36 -0400

    Create InteractionSystem.cs

---
+++ InteractionSystem.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class InteractionSystem : MonoBehaviour
+{
+    public float interactDistance = 3f;
+    public LayerMask interactLayer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, interactDistance, interactLayer))
+            {
+                IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+
+                if (interactable != null)
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+}
